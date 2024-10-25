1c93b22365536ba8ddad2180199fc4d16d93e4cd
Paolo Celati <paolo@celati.com>
2024-10-25 11:08:10 +0000

    variants, benchmarks and the whole project can now use a dphpc_md.json file to give test.py specific options.
    Also generalised N/TSTEPS so one passes the defines instead

---
+++ options.py
@@ -0,0 +1,50 @@
+from copy import deepcopy
+from dataclasses import dataclass
+import functools
+import json
+from pathlib import Path
+from typing import Any, List, Optional
+
+
+@dataclass
+class Options:
+    defines: Optional[dict[str, str]] = None
+    extra_compile_options: Optional[List[str]] = None
+
+    def __getitem__(self, item: str) -> Any:
+        return getattr(self, item)
+
+    def __setitem__(self, item: str, value: Any) -> None:
+        setattr(self, item, value)
+
+
+DefaultOptions = Options(defines={}, extra_compile_options=[])
+
+
+def options_from_file(fp: Path) -> Options:
+    ret = DefaultOptions
+    with open(fp, "r") as src:
+        j = json.load(src)
+        for param in Options.__dataclass_fields__:
+            if param in j:
+                ret[param] = j[param]
+    return ret
+
+
+def override_options(parent: Options, child: Options) -> Options:
+    ret = deepcopy(parent)
+    for param in Options.__dataclass_fields__:
+        if child[param] is not None:
+            if type(child[param]) == list:
+                ret[param] = parent[param] + child[param]
+            elif type(child[param]) == dict:
+                ret[param].update(child[param])
+            else:
+                ret[param] = child[param]
+    return ret
+
+
+def options_from_multiple_files(fps: List[Path]) -> Options:
+    # give it a list of filepaths and later filepaths will override prior ones
+    read_options = map(options_from_file, fps)
+    return functools.reduce(override_options, read_options, DefaultOptions)
