ee57f4b3dea415ffb87e37d9f8534af89b0b2c49
Paolo Celati <paolo@celati.com>
2024-12-02 09:10:56 +0000

    ADI multithreaded13 based off multithreaded12 but slow

--- adi/cuda_multithreaded13/cuda_impl.cu
+++ adi/cuda_multithreaded13/cuda_impl.cu
@@ -3,11 +3,14 @@
 #include <polybench.h>
 
 /* Include benchmark-specific header. */
+#include <cuda_profiler_api.h>
+
 #include <cuda_support.cuh>
 
 #include "adi.h"
 
 typedef struct {
+    default_adi_data_t adi_data;
     cublasStatus_t stat;
     cublasHandle_t handle;
     DATA_TYPE *u_dev;
@@ -15,9 +18,7 @@ typedef struct {
     DATA_TYPE *p_dev;
     DATA_TYPE *q_dev;
     DATA_TYPE *spare_arr;
-} kernel_init_t;
-
-kernel_init_t device_addrs;
+} cuda_adi_data_t;
 
 void transpose_in_place(DATA_TYPE POLYBENCH_2D(arr, N2, N2, n, n)) {
     for (int i = 0; i < N2; i++) {
@@ -30,11 +31,10 @@ void transpose_in_place(DATA_TYPE POLYBENCH_2D(arr, N2, N2, n, n)) {
     }
 }
 
-void transpose_oop(double *src_arr, double *dst_arr, int n) {
+inline void transpose_oop(cublasHandle_t handle, double *src_arr, double *dst_arr, int n) {
     double c1 = 1;
     double c0 = 0;
-    gpuCublasErrchk(
-        cublasDgeam(device_addrs.handle, CUBLAS_OP_T, CUBLAS_OP_N, n, n, &c1, src_arr, n, &c0, src_arr, n, dst_arr, n));
+    gpuCublasErrchk(cublasDgeam(handle, CUBLAS_OP_T, CUBLAS_OP_N, n, n, &c1, src_arr, n, &c0, src_arr, n, dst_arr, n));
 }
 
 void copy_array_transposed(DATA_TYPE POLYBENCH_2D(arr, N2, N2, n, n), double *device_addr, cudaMemcpyKind dir) {
@@ -48,76 +48,70 @@ void copy_array_transposed(DATA_TYPE POLYBENCH_2D(arr, N2, N2, n, n), double *de
     }
 }
 
-void initialise_benchmark(int argc, char **argv, int tsteps, int n, DATA_TYPE POLYBENCH_2D(u, N2, N2, n, n),
-                          DATA_TYPE POLYBENCH_2D(v, N2, N2, n, n), DATA_TYPE POLYBENCH_2D(p, N2, N2, n, n),
-                          DATA_TYPE POLYBENCH_2D(q, N2, N2, n, n)) {
-    (void)tsteps;
-    (void)n;
-    (void)argc;
-    (void)argv;
+void initialise_benchmark(int argc, char **argv, int tsteps, int n, void **gen_data_ptr) {
+    *gen_data_ptr = polybench_alloc_data(1, sizeof(cuda_adi_data_t));
+    cuda_adi_data_t *data_ptr = (cuda_adi_data_t *)(*gen_data_ptr);
+    default_initialise_benchmark(argc, argv, tsteps, n, &data_ptr->adi_data);
 
-    int i, j;
+    gpuErrchk(cudaMalloc(&data_ptr->u_dev, sizeof(DATA_TYPE) * n * n));
+    gpuErrchk(cudaMalloc(&data_ptr->v_dev, sizeof(DATA_TYPE) * n * n));
+    gpuErrchk(cudaMalloc(&data_ptr->p_dev, sizeof(DATA_TYPE) * n * n));
+    gpuErrchk(cudaMalloc(&data_ptr->q_dev, sizeof(DATA_TYPE) * n * n));
+    gpuErrchk(cudaMalloc(&data_ptr->spare_arr, sizeof(DATA_TYPE) * n * n));
 
-    for (i = 0; i < n; i++)
-        for (j = 0; j < n; j++) {
-            u[i][j] = (DATA_TYPE)(i + n - j) / n;
-        }
-    gpuErrchk(cudaMalloc(&device_addrs.u_dev, sizeof(DATA_TYPE) * n * n));
-    gpuErrchk(cudaMalloc(&device_addrs.v_dev, sizeof(DATA_TYPE) * n * n));
-    gpuErrchk(cudaMalloc(&device_addrs.p_dev, sizeof(DATA_TYPE) * n * n));
-    gpuErrchk(cudaMalloc(&device_addrs.q_dev, sizeof(DATA_TYPE) * n * n));
-    gpuErrchk(cudaMalloc(&device_addrs.spare_arr, sizeof(DATA_TYPE) * n * n));
+    gpuCublasErrchk(cublasCreate(&data_ptr->handle));
 
-    gpuCublasErrchk(cublasCreate(&device_addrs.handle));
-
-    gpuErrchk(cudaMemcpy(device_addrs.u_dev, u, sizeof(DATA_TYPE) * n * n, cudaMemcpyHostToDevice));
-    copy_array_transposed(v, device_addrs.v_dev, cudaMemcpyHostToDevice);
-    copy_array_transposed(p, device_addrs.p_dev, cudaMemcpyHostToDevice);
-    copy_array_transposed(q, device_addrs.q_dev, cudaMemcpyHostToDevice);
+    gpuErrchk(cudaMemcpy(data_ptr->u_dev, data_ptr->adi_data.u, sizeof(DATA_TYPE) * n * n, cudaMemcpyHostToDevice));
+    copy_array_transposed(data_ptr->adi_data.v, data_ptr->v_dev, cudaMemcpyHostToDevice);
+    copy_array_transposed(data_ptr->adi_data.p, data_ptr->p_dev, cudaMemcpyHostToDevice);
+    copy_array_transposed(data_ptr->adi_data.q, data_ptr->q_dev, cudaMemcpyHostToDevice);
+    gpuErrchk(cudaDeviceSynchronize());
+    cudaProfilerStart();
 }
 
-void finish_benchmark(int tsteps, int n, DATA_TYPE POLYBENCH_2D(u, N2, N2, n, n),
-                      DATA_TYPE POLYBENCH_2D(v, N2, N2, n, n), DATA_TYPE POLYBENCH_2D(p, N2, N2, n, n),
-                      DATA_TYPE POLYBENCH_2D(q, N2, N2, n, n)) {
-    (void)tsteps;
-    (void)n;
-
-    gpuErrchk(cudaMemcpy(u, device_addrs.u_dev, sizeof(DATA_TYPE) * n * n, cudaMemcpyDeviceToHost));
-    copy_array_transposed(v, device_addrs.v_dev, cudaMemcpyDeviceToHost);
-    copy_array_transposed(p, device_addrs.p_dev, cudaMemcpyDeviceToHost);
-    copy_array_transposed(q, device_addrs.q_dev, cudaMemcpyDeviceToHost);
-
-    gpuErrchk(cudaFree(device_addrs.u_dev));
-    gpuErrchk(cudaFree(device_addrs.v_dev));
-    gpuErrchk(cudaFree(device_addrs.p_dev));
-    gpuErrchk(cudaFree(device_addrs.q_dev));
-    gpuErrchk(cudaFree(device_addrs.spare_arr));
-    gpuCublasErrchk(cublasDestroy(device_addrs.handle));
+void finish_benchmark(void *gen_data_ptr) {
+    cudaProfilerStop();
+    cuda_adi_data_t *data_ptr = (cuda_adi_data_t *)gen_data_ptr;
+    int n = data_ptr->adi_data.n;
+    gpuErrchk(cudaMemcpy(data_ptr->adi_data.u, data_ptr->u_dev, sizeof(DATA_TYPE) * n * n, cudaMemcpyDeviceToHost));
+    copy_array_transposed(data_ptr->adi_data.v, data_ptr->v_dev, cudaMemcpyDeviceToHost);
+    copy_array_transposed(data_ptr->adi_data.p, data_ptr->p_dev, cudaMemcpyDeviceToHost);
+    copy_array_transposed(data_ptr->adi_data.q, data_ptr->q_dev, cudaMemcpyDeviceToHost);
 }
 
-__global__ void zero_out_arr_firstlast(int n, double *out_arr) {
-    for (int i = 1 + blockDim.x * blockIdx.x + threadIdx.x; i < _PB_N - 1; i += gridDim.x * blockDim.x) {
-        out_arr[0 * n + i] = SCALAR_VAL(1.0);
-        out_arr[(_PB_N - 1) * n + i] = SCALAR_VAL(1.0);
-    }
+void print_data(int argc, char **argv, int n, void *gen_data_ptr) {
+    default_print_data(argc, argv, n, &((cuda_adi_data_t *)gen_data_ptr)->adi_data);
+}
+
+void free_data(void *gen_data_ptr) {
+    cuda_adi_data_t *data_ptr = (cuda_adi_data_t *)gen_data_ptr;
+    gpuErrchk(cudaFree(data_ptr->u_dev));
+    gpuErrchk(cudaFree(data_ptr->v_dev));
+    gpuErrchk(cudaFree(data_ptr->p_dev));
+    gpuErrchk(cudaFree(data_ptr->q_dev));
+    gpuErrchk(cudaFree(data_ptr->spare_arr));
+    gpuCublasErrchk(cublasDestroy(data_ptr->handle));
+    free(gen_data_ptr);
 }
 
 inline __device__ void thread_sweep_footer(double *out_arr, int n, int i, double *p, double *q) {
     DATA_TYPE prev_out = SCALAR_VAL(1.0);
+    out_arr[0 * n + i] = SCALAR_VAL(1.0);
     for (int j = _PB_N - 2; j >= 1; j--) {
         prev_out = p[j * n + i] * prev_out + q[j * n + i];
         out_arr[j * n + i] = prev_out;
     }
+    out_arr[(_PB_N - 1) * n + i] = SCALAR_VAL(1.0);
 }
 
-#define SWEEP_TPB 30
-inline __device__ void sweep_generic(int n, double *in_arr, double *out_arr, double *p, double *q, double v1, double v2,
-                                     double v3, double v4, double v5) {
+#define SWEEP_TPB 1024
+__device__ void sweep_generic(int n, const double *in_arr, double *out_arr, double *p, double *q, double v1, double v2,
+                              double v3, double v4, double v5) {
     // col_sweep: in_arr = u, out_arr = v, v1=a, v2=b, v3=c, v4=d, v5=f
     // row_sweep: in_arr = v, out_arr = u, v1=d, v2=e, v3=f, v4=a, v5=c
 
     /*
-    bandwidth estimates:
+    min bandwidth estimates per timestep:
     load in_arr (N^2)
     store p and q (each N^2)
     load p and q (each N^2)
@@ -125,12 +119,20 @@ inline __device__ void sweep_generic(int n, double *in_arr, double *out_arr, dou
     */
 
     const int threads_in_block = SWEEP_TPB;
+    const int threads_in_block_plus_boundaries = threads_in_block;
     const int total_smem_slots = 48 * 1024 / sizeof(DATA_TYPE);
-    const int js_at_time = total_smem_slots / (threads_in_block + 2);
+    const int js_at_time = total_smem_slots / threads_in_block_plus_boundaries;
+    // const int js_at_time = 6;
 
-    __shared__ DATA_TYPE smem_in_arr[js_at_time][threads_in_block + 2];
+    __shared__ DATA_TYPE smem_in_arr[threads_in_block_plus_boundaries * js_at_time];
 
-    for (int i = 1 + blockDim.x * blockIdx.x + threadIdx.x; i < _PB_N - 1; i += gridDim.x * blockDim.x) {
+    int base_iter = 1;
+    int end_iter_excl = _PB_N - 1;
+    for (int i = base_iter + blockDim.x * blockIdx.x + threadIdx.x; i < end_iter_excl; i += gridDim.x * blockDim.x) {
+        int block_start_i = i - ((i - base_iter) % SWEEP_TPB);
+        int block_end_i = block_start_i + SWEEP_TPB;
+        int active_threads_this_block = block_end_i >= end_iter_excl ? end_iter_excl - block_start_i : SWEEP_TPB;
+
         DATA_TYPE prev_p = SCALAR_VAL(0.0);
         p[0 * n + i] = prev_p;
         DATA_TYPE prev_q = SCALAR_VAL(1.0);
@@ -138,32 +140,35 @@ inline __device__ void sweep_generic(int n, double *in_arr, double *out_arr, dou
 
         // copy in_arr for next few iterations into smem
 
-        int init_j = 1;
-        int j_ctr = 0;
+        const int init_j = 1;
+        int j_ctr = js_at_time;
         for (int j = init_j; j < _PB_N - 1; j++) {
-            if (j_ctr == 0 || j_ctr == js_at_time) {
-                // thread with index 0 should write to 0 and 1, with index i to i+1, and index threads_in_block to
-                // threads_in_block and threads_in_block+1
-                for (int j_temp = 0; j_temp < _PB_N - 1 && j_temp < js_at_time; j_temp++) {
-                    smem_in_arr[threadIdx.x + 1][j_temp] = in_arr[(j + j_temp) * n + i];
-                    if (threadIdx.x == 0) {
-                        smem_in_arr[threadIdx.x][j_temp] = in_arr[(j + j_temp) * n + i - 1];
-                    } else if (threadIdx.x == threads_in_block - 1) {
-                        smem_in_arr[threadIdx.x + 2][j_temp] = in_arr[(j + j_temp) * n + i + 1];
-                    }
+#define SMEM_ADDR(thread, j_temp) ((thread) * js_at_time + (j_temp))
+            // #define SMEM_ADDR(thread, j_temp) ((j_temp) * SWEEP_TPB + (thread))
+            if (j_ctr == js_at_time) {
+                const int js_to_load = _PB_N - 1 - j >= js_at_time ? js_at_time : _PB_N - 1 - j;
+                __syncthreads();  // avoid overwriting a slowpoke's shared memory
+                for (int j_temp = 0; j_temp < js_to_load && j_temp < js_at_time; j_temp++) {
+                    // smem_in_arr[j_temp * SWEEP_TPB + threadIdx.x] =
+                    smem_in_arr[SMEM_ADDR(threadIdx.x, j_temp)] =
+                        in_arr[(j + j_temp) * n + i];  // note this i is >= 1, not >= 0
                 }
-                j_ctr = 0;
                 __syncthreads();  // needed to ensure my neighbour threads don't go ahead until my data for them is
-                                  // available.
+                                  //   available.
+                j_ctr = 0;
             }
 
             DATA_TYPE new_prev_p = -v3 / (v1 * prev_p + v2);
 
-            DATA_TYPE inm1 = smem_in_arr[j_ctr][threadIdx.x];
-            DATA_TYPE in0 = smem_in_arr[j_ctr][threadIdx.x + 1];
-            DATA_TYPE inp1 = smem_in_arr[j_ctr][threadIdx.x + 2];
-
-            j_ctr++;
+            // only edges of block will use global memory because it simplifies storage into shared memory
+            DATA_TYPE inm1 = threadIdx.x == 0 ? in_arr[j * n + i - 1] : smem_in_arr[SMEM_ADDR(threadIdx.x - 1, j_ctr)];
+            DATA_TYPE in0 = smem_in_arr[SMEM_ADDR(threadIdx.x, j_ctr)];
+            DATA_TYPE inp1 = threadIdx.x == active_threads_this_block - 1
+                                 ? in_arr[j * n + i + 1]
+                                 : smem_in_arr[SMEM_ADDR(threadIdx.x + 1, j_ctr)];
+            // DATA_TYPE inm1 = in_arr[j * n + (i - 1)];
+            // DATA_TYPE in0 = in_arr[j * n + i];
+            // DATA_TYPE inp1 = in_arr[j * n + (i + 1)];
 
             DATA_TYPE new_prev_q =
                 (-v4 * inm1 + (SCALAR_VAL(1.0) + SCALAR_VAL(2.0) * v4) * in0 - v5 * inp1 - v1 * prev_q) /
@@ -173,18 +178,20 @@ inline __device__ void sweep_generic(int n, double *in_arr, double *out_arr, dou
             q[j * n + i] = new_prev_q;
             prev_p = new_prev_p;
             prev_q = new_prev_q;
+
+            j_ctr++;
         }
 
         thread_sweep_footer(out_arr, n, i, p, q);
     }
 }
 
-__global__ void col_sweep(int tsteps, int n, DATA_TYPE *u, DATA_TYPE *v, DATA_TYPE *p, DATA_TYPE *q, DATA_TYPE a,
+__global__ void col_sweep(int tsteps, int n, const DATA_TYPE *u, DATA_TYPE *v, DATA_TYPE *p, DATA_TYPE *q, DATA_TYPE a,
                           DATA_TYPE b, DATA_TYPE c, DATA_TYPE d, DATA_TYPE f) {
     sweep_generic(n, u, v, p, q, a, b, c, d, f);
 }
 
-__global__ void row_sweep(int tsteps, int n, DATA_TYPE *u, DATA_TYPE *v, DATA_TYPE *p, DATA_TYPE *q, DATA_TYPE a,
+__global__ void row_sweep(int tsteps, int n, DATA_TYPE *u, const DATA_TYPE *v, DATA_TYPE *p, DATA_TYPE *q, DATA_TYPE a,
                           DATA_TYPE c, DATA_TYPE d, DATA_TYPE e, DATA_TYPE f) {
     sweep_generic(n, v, u, p, q, d, e, f, a, c);
 }
@@ -195,7 +202,8 @@ __global__ void row_sweep(int tsteps, int n, DATA_TYPE *u, DATA_TYPE *v, DATA_TY
  * "Automatic Data and Computation Decomposition on Distributed Memory Parallel
  * Computers" by Peizong Lee and Zvi Meir Kedem, TOPLAS, 2002
  */
-void kernel_adi_inner(int tsteps, int n, DATA_TYPE *u, DATA_TYPE *v, DATA_TYPE *p, DATA_TYPE *q) {
+void kernel_adi_inner(cublasHandle_t handle, int tsteps, int n, DATA_TYPE *u, DATA_TYPE *v, DATA_TYPE *p, DATA_TYPE *q,
+                      DATA_TYPE *spare_arr) {
     DATA_TYPE DX, DY, DT;
     DATA_TYPE B1, B2;
     DATA_TYPE mul1, mul2;
@@ -217,33 +225,38 @@ void kernel_adi_inner(int tsteps, int n, DATA_TYPE *u, DATA_TYPE *v, DATA_TYPE *
     f = d;
 
     int sms = get_device_multiprocessors(0);
+
+    // should be able to zero out only once during program run
+
     for (int t = 1; t <= _PB_TSTEPS; t++) {
         // Column Sweep
         // Using very high block/thread count to give CUDA lots of independent work
         // threads have negligible memory needs so 1024/block makes sense.
-        double *temp_v = device_addrs.spare_arr;
-        // col_sweep writes to v but never reads from it, so can drop the old v entirely
-        // transpose_oop(v, temp_v,
-        //               n);  // for some reason transpose_oop is needed before sweep else a race condition occurs
-        col_sweep<<<sms, SWEEP_TPB, 48 * 1024>>>(tsteps, n, u, temp_v, p, q, a, b, c, d, f);
-        if (t == _PB_TSTEPS) {
-            zero_out_arr_firstlast<<<sms, 1024>>>(n, temp_v);
-        }
-        transpose_oop(temp_v, v, n);
+        double *temp_v = spare_arr;
+        // col_sweep writes to v but never reads from it, so can drop the old v entirely except first/last row.
+        transpose_oop(handle, v, temp_v, n);
+        col_sweep<<<sms, SWEEP_TPB>>>(tsteps, n, u, temp_v, p, q, a, b, c, d, f);
+        transpose_oop(handle, temp_v, v, n);
+        // if (t == 1) {
+        //     zero_out_arr_firstlast_cols<<<sms, 1024>>>(n, v);
+        // }
+
         // Row Sweep
-        double *temp_u = device_addrs.spare_arr;
+        double *temp_u = spare_arr;
         // row_sweep only writes to u but never reads so no need to initialise temp_u.
-        // transpose_oop(u, temp_u, n);
-        row_sweep<<<sms, SWEEP_TPB, 48 * 1024>>>(tsteps, n, temp_u, v, p, q, a, c, d, e, f);
-        if (t == _PB_TSTEPS) {
-            zero_out_arr_firstlast<<<sms, 1024>>>(n, temp_u);
-        }
-        transpose_oop(temp_u, u, n);
+        // currently transposing whole u but only really care about first/last rows.
+        transpose_oop(handle, u, temp_u, n);
+        row_sweep<<<sms, SWEEP_TPB>>>(tsteps, n, temp_u, v, p, q, a, c, d, e, f);
+        transpose_oop(handle, temp_u, u, n);
+        // if (t == 1) {
+        //     zero_out_arr_firstlast_cols<<<sms, 1024>>>(n, u);
+        // }
     }
 }
 
-void kernel_adi(int tsteps, int n, DATA_TYPE POLYBENCH_2D(u, N2, N2, n, n), DATA_TYPE POLYBENCH_2D(v, N2, N2, n, n),
-                DATA_TYPE POLYBENCH_2D(p, N2, N2, n, n), DATA_TYPE POLYBENCH_2D(q, N2, N2, n, n)) {
-    kernel_adi_inner(tsteps, n, device_addrs.u_dev, device_addrs.v_dev, device_addrs.p_dev, device_addrs.q_dev);
+void kernel_adi(void *gen_data_ptr) {
+    cuda_adi_data_t *data_ptr = (cuda_adi_data_t *)gen_data_ptr;
+    kernel_adi_inner(data_ptr->handle, data_ptr->adi_data.tsteps, data_ptr->adi_data.n, data_ptr->u_dev,
+                     data_ptr->v_dev, data_ptr->p_dev, data_ptr->q_dev, data_ptr->spare_arr);
     gpuErrchk(cudaDeviceSynchronize());
 }
