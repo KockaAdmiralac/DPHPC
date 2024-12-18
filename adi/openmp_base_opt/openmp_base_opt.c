cd3ed90c2269a8283d7499f17514e1307338b1e1
Giorgos Drosos <t8180024@aueb.gr>
2024-12-18 21:05:18 +0100

    Fix formatting

--- adi/openmp_base_opt/openmp_base_opt.c
+++ adi/openmp_base_opt/openmp_base_opt.c
@@ -56,8 +56,7 @@ void kernel_adi_orig(int tsteps, int n, DATA_TYPE POLYBENCH_2D(u, N2, N2, n, n),
                 denom_inv = SCALAR_VAL(1.0) / (a * p[i][j - 1] + b);
                 p[i][j] = -c * denom_inv;
                 q[i][j] =
-                    (-d * u[j][i - 1] + const_1_2d * u[j][i] - f * u[j][i + 1] + const_neg_a * q[i][j - 1]) *
-                    denom_inv;
+                    (-d * u[j][i - 1] + const_1_2d * u[j][i] - f * u[j][i + 1] + const_neg_a * q[i][j - 1]) * denom_inv;
             }
             v[_PB_N - 1][i] = SCALAR_VAL(1.0);
             for (j = _PB_N - 2; j >= 1; j--) {
@@ -74,7 +73,9 @@ void kernel_adi_orig(int tsteps, int n, DATA_TYPE POLYBENCH_2D(u, N2, N2, n, n),
             for (int j = 1; j < _PB_N - 1; j++) {
                 denom_inv = SCALAR_VAL(1.0) / (d * p[i][j - 1] + e);
                 p[i][j] = -f * denom_inv;
-                q[i][j] = (const_neg_a * v[i - 1][j] + const_1_2a * v[i][j] + const_neg_c * v[i + 1][j] - d * q[i][j - 1]) * denom_inv;
+                q[i][j] =
+                    (const_neg_a * v[i - 1][j] + const_1_2a * v[i][j] + const_neg_c * v[i + 1][j] - d * q[i][j - 1]) *
+                    denom_inv;
             }
             u[i][_PB_N - 1] = SCALAR_VAL(1.0);
             for (j = _PB_N - 2; j >= 1; j--) {
--- adi/openmp_base_split_loop/openmp_split_loop.c
+++ adi/openmp_base_split_loop/openmp_split_loop.c
@@ -43,9 +43,7 @@ void kernel_adi_orig(int tsteps, int n, DATA_TYPE POLYBENCH_2D(u, N2, N2, n, n),
             q[i][0] = v[0][i];
             for (j = 1; j < _PB_N - 1; j++) {
                 p[i][j] = -c / (a * p[i][j - 1] + b);
-                q[i][j] = (-d * u[j][i - 1] + (SCALAR_VAL(1.0) + SCALAR_VAL(2.0) * d) * u[j][i] - f * u[j][i + 1] -
-                           a * q[i][j - 1]) /
-                          (a * p[i][j - 1] + b);
+                q[i][j] = (-d * u[j][i - 1] + (SCALAR_VAL(1.0) + SCALAR_VAL(2.0) * d) * u[j][i] - f * u[j][i + 1] - a * q[i][j - 1]) / (a * p[i][j - 1] + b);
             }
         }
         #pragma omp parallel for private(j)
