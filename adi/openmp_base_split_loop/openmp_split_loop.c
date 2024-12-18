b418ea52abe95cb744a22c7de139a30b6cff500f
Giorgos Drosos <t8180024@aueb.gr>
2024-12-18 21:07:11 +0100

    Some neats

--- adi/openmp_base_split_loop/openmp_split_loop.c
+++ adi/openmp_base_split_loop/openmp_split_loop.c
@@ -43,10 +43,12 @@ void kernel_adi_orig(int tsteps, int n, DATA_TYPE POLYBENCH_2D(u, N2, N2, n, n),
             q[i][0] = v[0][i];
             for (j = 1; j < _PB_N - 1; j++) {
                 p[i][j] = -c / (a * p[i][j - 1] + b);
-                q[i][j] = (-d * u[j][i - 1] + (SCALAR_VAL(1.0) + SCALAR_VAL(2.0) * d) * u[j][i] - f * u[j][i + 1] - a * q[i][j - 1]) / (a * p[i][j - 1] + b);
+                q[i][j] = (-d * u[j][i - 1] + (SCALAR_VAL(1.0) + SCALAR_VAL(2.0) * d) * u[j][i] - f * u[j][i + 1] -
+                           a * q[i][j - 1]) /
+                          (a * p[i][j - 1] + b);
             }
         }
-        #pragma omp parallel for private(j)
+#pragma omp parallel for private(j)
         // // Backward Pass (Sequential)
         for (int i = 1; i < _PB_N - 1; i++) {
             v[_PB_N - 1][i] = SCALAR_VAL(1.0);
@@ -67,7 +69,7 @@ void kernel_adi_orig(int tsteps, int n, DATA_TYPE POLYBENCH_2D(u, N2, N2, n, n),
                           (d * p[i][j - 1] + e);
             }
         }
-        #pragma omp parallel for private(j)
+#pragma omp parallel for private(j)
         for (int i = 1; i < _PB_N - 1; i++) {
             u[i][_PB_N - 1] = SCALAR_VAL(1.0);
             for (j = _PB_N - 2; j >= 1; j--) {
